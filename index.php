4e97260e55a18f0b2863cfb336b86a55ddddc08a
aoleiC <804834396@qq.com>
2018-11-05 14:36:13 +0800

    init

--- index.php
+++ index.php
@@ -6,38 +6,48 @@ error_reporting(E_ALL & ~E_NOTICE);  //error_reporting(0);
 * 输出到文件
 * param string $data 字符串
 */
-function console($data)
-{
+function console($data) {
+	if(!$data) return;
     $myfile = fopen("console.log", "a") or die("Unable to open file!");
     fwrite($myfile, date('Y-m-d H:i:s  ') . "\n");
     fwrite($myfile, $data . "\n");
     fclose($myfile);
 }
 
+function end_line() {
+	$myfile = fopen("console.log", "a") or die("Unable to open file!");
+    fwrite($myfile, "-------------------↑↑ end ↑↑---------------------------\n\n");
+    fclose($myfile);
+	exit();
+}
+
 $token = $_REQUEST['token'];
 $type = $_REQUEST['type'];
 
+console(json_encode($_SERVER));
+
 // 配置执行的 shell 脚本
 $config = [
-    'code' => 'sh code.sh',
+    'code' => 'sh test.sh',
     'docker' => 'cd /data/shell && sh docker.sh'
 ];
 
 // 简单 token 验证，根据需要修改
 if ($token !== '7b6a7a9c8066859f69ee5019b3675869') {
-    console('error token');
-    exit();
+    console('error token ' . $token);
+	end_line();
 }
 
 if (!$type || !$config[$type]) {
-    console('error type' . $type);
-    exit();
+    console('error type ' . $type);
+	end_line();
 }
 
 if (function_exists('shell_exec')) {
     $output = shell_exec($config[$type]);
     console($output);
     console($_SERVER['REMOTE_ADDR'] . ":" . $_SERVER['REMOTE_PORT'] . " " . $_SERVER['REQUEST_URI']);
-    echo 'success';
+	echo 'success';
+	end_line();
 }
 
